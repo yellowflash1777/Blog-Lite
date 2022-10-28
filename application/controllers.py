c102ce404d7629f93cd1672f24d970aeef789337
Prashant Prithyani <prithyani2002@gmail.com>
2022-10-28 10:45:16 +0530

    added login

--- application/controllers.py
+++ application/controllers.py
@@ -1,15 +1,39 @@
 
 from flask import current_app as app, redirect, render_template, request, url_for
+from .database import db
+from application.models import User
 
 @app.route('/', methods=["GET", "POST"])
-def index():
+def login():
     if request.method == "POST": 
         username=request.form.get("username")
         password=request.form.get("password")
-        if username == "admin" and password == "admin":
-           return redirect(url_for('admin')) 
-    return render_template('index.html')
+        user = User.query.filter_by(username=username,password=password).first()
+        if user is not None:
+            return redirect(url_for('home', username=username))
+        else:
+            return render_template("login.html", message="Invalid username and/or password")
+    return render_template("login.html")
 
-@app.route('/admin')
-def admin():
-    return render_template('admin.html')
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username=request.form.get("username")
+        password=request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            user = User(username=username,password=password)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('home', username=username))
+        else:
+            return render_template("register.html", message="Username already exists")
+    return render_template("register.html")
+
+
+
+
+@app.route('/home')
+def home():
+    username=request.args.get("username")
+    return render_template('home.html',username=username)
